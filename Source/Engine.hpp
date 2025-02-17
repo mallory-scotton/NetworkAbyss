859ba265b34a9c31fd2328d5b6917d131eecc83d
mallory-scotton <mallory.scotton@epitech.eu>
2025-02-17 17:58:38 +0100

    add: adding every placeholder files

---
+++ Source/Engine.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Engine/Core.hpp>
+#include <Engine/Network.hpp>
+#include <Engine/Resources.hpp>
+#include <Engine/Utils.hpp>
---
+++ Source/Engine/Resources/AssetsManager.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace tkd
+{
+
+class AssetsManager
+{
+
+};
+
+}
