8cf1067f4746e7be04c37d00198b2a059610ab9d
mallory-scotton <mallory.scotton@epitech.eu>
2025-02-17 18:23:01 +0100

    add: adding main

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,9 +3,33 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include <Engine.hpp>
 #include <Game.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
 ///////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
+    sf::RenderWindow* window = new sf::RenderWindow(
+        sf::VideoMode({800, 600}), "NetworkAbyss", sf::Style::Close
+    );
+
+    while (window->isOpen()) {
+        while (const std::optional event = window->pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window->close();
+            } else if (const auto* key = event->getIf<sf::Event::KeyPressed>()) {
+                if (key->scancode == sf::Keyboard::Scancode::Escape)
+                    window->close();
+            }
+        }
+
+        window->display();
+
+        window->clear();
+    }
+
+    delete window;
+
     return (EXIT_SUCCESS);
 }
